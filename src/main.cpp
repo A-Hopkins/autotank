c19fa46d50076590051cf26fb0ce7627db5a072e
a-hopkins <hopkinsalex@knights.ucf.edu>
2023-12-30 18:21:37 -0600

    uncommented code in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,56 +5,50 @@
 
 int main(void)
 {
-  // int i;
-  // dc_motor motor_1(1);
-  // dc_motor motor_2(2);
-
-  // motor_1.change_state(dc_motor::FORWARD);
-  // motor_1.set_speed(255);
-
-  // motor_2.change_state(dc_motor::FORWARD);
-  // motor_2.set_speed(200);
+  int i;
+  dc_motor motor_1(1);
+  dc_motor motor_2(2);
 
   while (true)
   {
-    // motor_1.change_state(dc_motor::FORWARD);
-    // motor_2.change_state(dc_motor::FORWARD);
+    motor_1.change_state(dc_motor::FORWARD);
+    motor_2.change_state(dc_motor::FORWARD);
 
-    // // Ramp up speed on both motors
-    // for (i=190; i < 256; i++)
-    // {
-    //   motor_1.set_speed(i);
-    //   motor_2.set_speed(i);
-    //   _delay_ms(100);
-    // }
-    // // Decrease speed on both motors
-    // for (i=256; i > 0; i--)
-    // {
-    //   motor_1.set_speed(i);
-    //   motor_2.set_speed(i);
-    //   _delay_ms(100);
-    // }
+    // Ramp up speed on both motors
+    for (i=190; i < 256; i++)
+    {
+      motor_1.set_speed(i);
+      motor_2.set_speed(i);
+      _delay_ms(100);
+    }
+    // Decrease speed on both motors
+    for (i=256; i > 0; i--)
+    {
+      motor_1.set_speed(i);
+      motor_2.set_speed(i);
+      _delay_ms(100);
+    }
 
-    // _delay_ms(1000);
-    // motor_1.change_state(dc_motor::REVERSE);
-    // motor_2.change_state(dc_motor::REVERSE);
-    // // Ramp up speed on both motors
-    // for (i=190; i < 256; i++)
-    // {
-    //   motor_1.set_speed(i);
-    //   motor_2.set_speed(i);
-    //   _delay_ms(100);
-    // }
-    // // Decrease speed on both motors
-    // for (i=256; i > 0; i--)
-    // {
-    //   motor_1.set_speed(i);
-    //   motor_2.set_speed(i);
-    //   _delay_ms(100);
-    // }
+    _delay_ms(1000);
+    motor_1.change_state(dc_motor::REVERSE);
+    motor_2.change_state(dc_motor::REVERSE);
+    // Ramp up speed on both motors
+    for (i=190; i < 256; i++)
+    {
+      motor_1.set_speed(i);
+      motor_2.set_speed(i);
+      _delay_ms(100);
+    }
+    // Decrease speed on both motors
+    for (i=256; i > 0; i--)
+    {
+      motor_1.set_speed(i);
+      motor_2.set_speed(i);
+      _delay_ms(100);
+    }
 
-    // motor_1.change_state(dc_motor::RELEASE);
-    // motor_2.change_state(dc_motor::RELEASE);
+    motor_1.change_state(dc_motor::RELEASE);
+    motor_2.change_state(dc_motor::RELEASE);
   }
 
   return 0;
